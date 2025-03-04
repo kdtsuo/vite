2b4a91f43e441f3f314718a49f4cf6dec748076c
rinwindows <phurinmeng@gmail.com>
2025-03-03 19:39:25 -0800

    refactor Navbar component to use React Router for navigation links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
 export default function Navbar() {
   return (
     <div className="bg-gray-800 text-white p-4 w-full">
       <h1 className="text-2xl font-bold">Navbar</h1>
       <div>
-        <a href="#" className="hover:underline">
-          Link 1
-        </a>
-        <a href="#" className="hover:underline ml-4">
-          Link 2
-        </a>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/contacts">Contacts</Link>
+          </li>
+        </ul>
       </div>
     </div>
   );
