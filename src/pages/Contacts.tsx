68deac863febe7f06c678215857b5df03d3ded53
rin! <phurinmeng@gmail.com>
2025-03-04 14:40:06 -0800

    add emailjs and emailjs-com dependencies; refactor form components for improved readability and consistency

--- src/pages/Contacts.tsx
+++ src/pages/Contacts.tsx
@@ -26,6 +26,7 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import * as z from "zod";
+import emailjs from "emailjs-com";
 
 // Define schema for form validation
 const formSchema = z.object({
@@ -40,13 +41,15 @@ const formSchema = z.object({
   }),
 });
 
+const service: string = "service_qii0r9i";
+const template: string = "template_se1ntd8";
+const user: string = "xA2mLRICgKakxEiNJ";
+
 // Define interface for social links
 interface SocialLink {
   icon: string;
-  alt: string;
   href: string;
   title: string;
-  description: string;
 }
 
 export default function Contacts() {
@@ -61,55 +64,79 @@ export default function Contacts() {
   });
 
   const onSubmit = async (values: z.infer<typeof formSchema>) => {
-    // Handle form submission
     console.log(values);
-    toast.success("Message sent!", {
-      description: "Thank you for your message. We'll get back to you soon.",
-    });
+
+    interface EmailJSTemplateParams {
+      from_name: string;
+      from_email: string;
+      message: string;
+      [key: string]: string; // Allow for additional string properties
+    }
+
+    const templateParams: EmailJSTemplateParams = {
+      from_name: values.name,
+      from_email: values.email,
+      message: values.message,
+    };
+
+    emailjs.send(service, template, templateParams, user).then(
+      (response) => {
+        console.log("SUCCESS!", response.status, response.text);
+        toast.success("Message sent!", {
+          description:
+            "Thank you for your message. We'll get back to you soon.",
+        });
+      },
+      (err) => {
+        console.log("FAILED...", err);
+
+        toast.error("Message failed to send!", {
+          description:
+            "Sorry, we were unable to send your message. Please try again later.",
+        });
+      }
+    );
     form.reset();
   };
 
   const socialLinks: SocialLink[] = [
     {
       icon: discordlogo,
-      alt: "Discord",
-      href: "https://discord.com",
+      href: "https://discord.com/invite/tbKkvjV2W8",
       title: "Discord",
-      description: "Join our community and stay connected",
     },
     {
       icon: githublogo,
-      alt: "GitHub",
-      href: "https://github.com",
+      href: "https://github.com/kdtsuo/kdtsuo.github.io-vite",
       title: "GitHub",
-      description: "Check out our open-source projects",
     },
     {
       icon: instagramlogo,
-      alt: "Instagram",
-      href: "https://instagram.com",
+      href: "https://www.instagram.com/kdt.suo/?theme=dark",
       title: "Instagram",
-      description: "Follow us for latest updates",
     },
     {
       icon: maillogo,
-      alt: "Email",
-      href: "mailto:contact@example.com",
+      href: "mailto:kpopdanceteam.suo@gmail.com",
       title: "Email",
-      description: "Reach out directly to our team",
     },
   ];
 
   return (
     <div
-      className="bg-lb-100 min-h-screen flex items-center justify-center p-4 
+      className="bg-lb-100 h-auto lg:h-screen w-full flex items-center justify-center pt-40 pb-20 lg:py-10
       bg-[radial-gradient(#ffffff_1px,transparent_1px)] [background-size:20px_20px]"
     >
-      <div className="w-4/5 max-w-6xl bg-white shadow-xl rounded-xl overflow-hidden flex relative">
+      <div
+        className="m-5 w-full max-w-6xl bg-white shadow-xl rounded-xl overflow-hidden 
+        flex flex-col lg:flex-row relative "
+      >
         {/* Social Media Cards Section */}
-        <div className="w-1/3 p-8 flex flex-col justify-between items-center">
-          <h2 className="text-3xl font-bold text-center">Connect With Us</h2>
-          <div className="w-full flex-grow justify-between flex flex-col py-6">
+        <div className="w-full lg:w-1/3 flex flex-col justify-center items-center p-12 py-6 lg:py-12">
+          <div className="w-full flex-grow justify-center flex flex-col py-8 space-y-2">
+            <h2 className="text-3xl font-bold text-center pb-2">
+              Connect With Us
+            </h2>
             {socialLinks.map((link, index) => (
               <a
                 key={index}
@@ -118,8 +145,12 @@ export default function Contacts() {
                 rel="noopener noreferrer"
               >
                 <Card className="hover:shadow-lg transition-all duration-200 hover:-translate-y-1 bg-black text-white">
-                  <CardHeader className="flex flex-row items-center space-x-4 p-4">
-                    <img src={link.icon} alt={link.alt} className="w-12 h-12" />
+                  <CardHeader className="flex flex-row items-center justify-between lg:justify-start space-x-4 p-4">
+                    <img
+                      src={link.icon}
+                      alt={link.title}
+                      className="w-12 h-12"
+                    />
                     <CardTitle className="text-xl font-extralight">
                       {link.title}
                     </CardTitle>
@@ -130,11 +161,12 @@ export default function Contacts() {
           </div>
         </div>
 
-        {/* Vertical line  */}
-        <div className="absolute left-1/3 top-0 bottom-0 w-0.5 bg-gray-300 my-8"></div>
+        {/* Divider Line */}
+        <div className="hidden lg:block absolute left-1/3 top-0 bottom-0 w-0.5 bg-gray-300 my-8"></div>
+        <div className="block lg:hidden w-full h-0.5 bg-gray-300 my-4"></div>
 
         {/* Contact Form Section */}
-        <div className="w-2/3 p-12">
+        <div className="w-full lg:w-2/3 p-12">
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
               <h2 className="text-3xl font-bold text-center mb-6">
