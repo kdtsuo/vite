97812f29739573bcd0d3975cf8be9494b8667fd9
rin! <phurinmeng@gmail.com>
2025-03-04 15:57:09 -0800

    update base path in Vite configuration for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/kdtsuo.github.io-vite",
+  base: "/kdtsuo.github.io",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
