1be70a9a6176ba15f83cee6c1316553194111cef
rin! <phurinmeng@gmail.com>
2025-03-04 16:31:25 -0800

    update base path in Vite configuration for local development

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/kdtsuo.github.io",
+  base: "/vite",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
