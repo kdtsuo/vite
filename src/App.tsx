17d79edb4fdfc8c9c35944de8b8f4f1a0d980eb4
rin! <phurinmeng@gmail.com>
2025-03-04 16:18:45 -0800

    fix routing paths and switch to HashRouter for improved navigation

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="sponsors" element={<Sponsors />} />
+        <Route path="/sponsors" element={<Sponsors />} />
         <Route path="*" element={<NotFound />} /> {/* Catch-all for 404s */}
       </Routes>
     </>
