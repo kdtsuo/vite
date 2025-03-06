e527e19debd1371a75871694124389633d83e2d9
rinwindows <phurinmeng@gmail.com>
2025-03-05 16:00:33 -0800

    restyle carousel

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -183,7 +183,7 @@ function CarouselPrevious({
       variant={variant}
       size={size}
       className={cn(
-        "absolute size-8 rounded-full",
+        "absolute size-8 rounded-full hover:opacity-75 cursor-pointer",
         orientation === "horizontal"
           ? "top-1/2 -left-12 -translate-y-1/2"
           : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
@@ -193,7 +193,7 @@ function CarouselPrevious({
       onClick={scrollPrev}
       {...props}
     >
-      <ArrowLeft strokeWidth={3} size={30} />
+      <ArrowLeft strokeWidth={3} size={30} className="text-black" />
       <span className="sr-only">Previous slide</span>
     </Button>
   );
@@ -213,7 +213,7 @@ function CarouselNext({
       variant={variant}
       size={size}
       className={cn(
-        "absolute size-8 rounded-full",
+        "absolute size-8 rounded-full hover:opacity-75 cursor-pointer",
         orientation === "horizontal"
           ? "top-1/2 -right-12 -translate-y-1/2"
           : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
@@ -223,7 +223,7 @@ function CarouselNext({
       onClick={scrollNext}
       {...props}
     >
-      <ArrowRight strokeWidth={3} size={30} />
+      <ArrowRight strokeWidth={3} size={30} className="text-black" />
       <span className="sr-only">Next slide</span>
     </Button>
   );
