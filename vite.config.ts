b90c2080c62a8c318d5d94daa125b70a46089b7b
rinwindows <phurinmeng@gmail.com>
2025-03-05 18:19:25 -0800

    add cross-env package and update build scripts for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/v3",
+  base: process.env.GITHUB_PAGES === "true" ? "/v3/" : "/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
