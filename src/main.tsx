de9a97c2913d698fe298b5050bbfbbacb4a13769
rin! <phurinmeng@gmail.com>
2025-03-07 15:35:21 -0800

    fix: remove import of unused index.css file from main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
 import "./custom.css";
 import { AuthProvider } from "@/contexts/AuthContext";
 import { ThemeProvider } from "@/contexts/ThemeContext";
